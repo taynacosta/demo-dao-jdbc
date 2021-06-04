b5f0a3a49a0907d8412b2b488f17c87af4222a19
taynacosta <taynamoreira1811@gmail.com>
2021-06-04 19:06:07 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,22 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import model.entites.Department;
+import model.entites.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "books");
 		
-		System.out.println(obj);
+		SimpleDateFormat sdf = new SimpleDateFormat(String.format("dd/MM/yyyy"));
+		
+		Department obj = new Department(1, "books");
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+		
+		System.out.println(seller);
 
 	}
 
