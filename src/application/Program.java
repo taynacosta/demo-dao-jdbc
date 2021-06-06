8a4f8a5d692d5e3dbfcbcb77c1c3c32dd15ce28b
taynacosta <taynamoreira1811@gmail.com>
2021-06-06 08:41:35 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,6 +16,7 @@ public class Program {
 		SimpleDateFormat sdf = new SimpleDateFormat(String.format("dd/MM/yyyy"));
 	
 		SellerDao sellerdao = DaoFactory.createSellerDao();
+		System.out.println("===TEST 1: seller findById ===");
 		Seller seller = sellerdao.findById(3);
 		
 		System.out.println(seller);
